167c8ddf8df75a60e69185a7fa86e655e5d591e3
Diego Silva <dssilva0710@hotmail.com>
2023-07-19 09:57:02 -0300

    Commit da primeira tarefa de git

---
+++ mod5/Primeiro-Projeto/src/br/com/dsilva/PrimeiraClasse.java
@@ -0,0 +1,9 @@
+package br.com.dsilva;
+
+public class PrimeiraClasse {
+
+    public static void main (String[] args) {
+        String name = "Diego";
+        System.out.print("Hello " + name);
+    }
+}
