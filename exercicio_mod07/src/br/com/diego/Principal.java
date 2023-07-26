c2efa5043db761271e24c56217793511e1d35a39
Diego Silva <dssilva0710@hotmail.com>
2023-07-26 12:20:00 -0300

    Envio da atividade do mod8

--- exercicio_mod07/src/br/com/diego/Principal.java
+++ exercicio_mod07/src/br/com/diego/Principal.java
@@ -4,6 +4,10 @@ package br.com.diego;
 import javax.print.attribute.standard.Media;
 public class Principal { // Classe principal para execução do método de cálculo dá média aritmetica
 
+    public int soma(int num1, int num2) {
+        return num1 + num2;
+    }
+
     /**
      *
      * @param args
@@ -12,5 +16,7 @@ public class Principal { // Classe principal para execução do método de cálc
 
         MediaAritmetica resultado = new MediaAritmetica();
         System.out.println("A média aritmética é: " + resultado.calculoMediaAritmetica(10, 7, 8));
+        Principal soma = new Principal();
+        System.out.println(soma.soma(10, 20));
     }
 }
